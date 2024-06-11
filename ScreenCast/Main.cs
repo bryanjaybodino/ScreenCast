73b27c160b9df753594e126cd4b228cf95b9e12f
Bryanjay Bodino <bryanjaybodino@gmail.com>
2024-06-11 11:20:10 +0800

    Add project files.

---
+++ ScreenCast/Main.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ScreenCast
+{
+    public partial class Main : Form
+    {
+        private TcpListener listener;
+        int port = 8090;
+        public Main()
+        {
+            InitializeComponent();
+        }
+        private void Start()
+        {
+            // Create a TCP listener on the specified port
+            listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            // Start accepting clients asynchronously
+            Task.Run(() => AcceptClients());
+        }
+
+        private void AcceptClients()
+        {
+            while (true)
+            {
+                try
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    Task.Run(() => SendScreenFrames(client));
+                }
+                catch
+                {
+                }
+            }
+        }
+
+
+        private void SendScreenFrames(TcpClient client)
+        {
+            const string Boundary = "MyFrame";// CHNAGE ITO PAG MULTIPLE SCREEN
+            using (NetworkStream stream = client.GetStream())
+            {
+                try
+                {
+                    string initialBoundary = "" +
+                      $"HTTP/1.1 200 OK\r\n" +
+                      $"Content-Type: multipart/x-mixed-replace; boundary=--{Boundary}\r\n\r\n";
+
+                    byte[] initialBoundaryBytes = Encoding.ASCII.GetBytes(initialBoundary);
+                    stream.Write(initialBoundaryBytes, 0, initialBoundaryBytes.Length);
+
+                    while (client.Connected)
+                    {
+                        CaptureScreen(stream, Boundary);
+
+                    }
+                }
+                catch { }
+            }
+        }
+
+        private void CaptureScreen(NetworkStream stream, string boundary)
+        {
+            try
+            {
+
+                //KINIKUHA MUNA NATIN YUNG PERCENTAGE NG SCREEN SETTING NG WINDOWS PARA TAMA BUO ANG MAKUHA SA SCREEN
+                double factor = ScreenPercentage.scale();
+                int width = ((int)(Screen.PrimaryScreen.Bounds.Width) * (int)(factor)) / 100;
+                int height = ((int)(Screen.PrimaryScreen.Bounds.Height) * (int)(factor) / 100);
+
+
+                //ILALAGAY NANATIN SA BITMAP YUNG SIZE NG IMAGE
+                using (var bmpScreenCapture = new Bitmap(width, height))
+                using (var graphics = Graphics.FromImage(bmpScreenCapture))
+                {
+                    int boundX = Screen.PrimaryScreen.Bounds.X;
+                    int boundY = Screen.PrimaryScreen.Bounds.Y;
+                    graphics.CopyFromScreen(boundX, boundY, 0, 0, bmpScreenCapture.Size, CopyPixelOperation.SourceCopy);
+
+                    //I SEND NA NATIN SA BROWSER
+                    using (var ms = new MemoryStream())
+                    {
+                        bmpScreenCapture.Save(ms, ImageFormat.Jpeg);
+                        string header = $"--{boundary}\r\n" +
+                                        $"Content-Type: image/jpeg\r\n" +
+                                        $"Content-Length: {ms.Length}\r\n\r\n";
+
+                        byte[] headerBytes = Encoding.ASCII.GetBytes(header);
+                        stream.WriteAsync(headerBytes, 0, headerBytes.Length);
+                        ms.Position = 0;
+                        ms.CopyTo(stream);
+                        stream.FlushAsync();
+                    }
+                }
+            }
+            catch { }
+        }
+
+        private void UpdateHTML()
+        {
+            //UPDATE HTML PARA SA DYNAMIC IP ADDRESS
+            string Ip = Dns.GetHostByName(Dns.GetHostName()).AddressList[0].ToString();
+            string path = Environment.CurrentDirectory.Replace("\\bin\\Debug", "") + "\\index.html";
+            string ImagePath = "http://" + Ip + ":" + port.ToString();
+            string image = "<img src=\"" + ImagePath + "\" style=\"\r\n        height: 100%;\r\n        bottom: 0;\r\n        left: 0;\r\n        margin: auto;\r\n        overflow: auto;\r\n        right: 0;\r\n        top: 0px;\r\n        -o-object-fit: contain;\r\n        object-fit: contain;\r\n        border: none;\r\n        border-width: 0px;\r\n        border-color: white;\r\n        -webkit-user-drag: none;\r\n        user-drag: none;\r\n        user-select: none;\r\n        pointer-events: none;\r\n        position:fixed;\"/>";
+            File.WriteAllText(path, image);
+            try
+            {
+                Task.Delay(1000);
+                Process.Start(path);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void Main_Load(object sender, EventArgs e)
+        {
+            Start(); 
+            UpdateHTML();
+        }
+    }
+}
---
+++ ScreenCast/ScreenPercentage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenCast
+{
+    public class ScreenPercentage
+    {
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int GetDeviceCaps(IntPtr hDC, int nIndex);
+
+        public enum DeviceCap
+        {
+            VERTRES = 10,
+            DESKTOPVERTRES = 117
+        }
+
+        //int myheight =;
+        public static double scale(bool percentage = true)
+        {
+            //Create Graphics object from the current windows handle
+            Graphics GraphicsObject = Graphics.FromHwnd(IntPtr.Zero);
+            //Get Handle to the device context associated with this Graphics object
+            IntPtr DeviceContextHandle = GraphicsObject.GetHdc();
+            //Call GetDeviceCaps with the Handle to retrieve the Screen Height
+            int LogicalScreenHeight = GetDeviceCaps(DeviceContextHandle, (int)DeviceCap.VERTRES);
+            int PhysicalScreenHeight = GetDeviceCaps(DeviceContextHandle, (int)DeviceCap.DESKTOPVERTRES);
+            //Divide the Screen Heights to get the scaling factor and round it to two decimals
+            double ScreenScalingFactor = Math.Round((double)PhysicalScreenHeight / (double)LogicalScreenHeight, 2);
+            //If requested as percentage - convert it
+            if (percentage)
+            {
+                ScreenScalingFactor *= 100.0;
+            }
+            //Release the Handle and Dispose of the GraphicsObject object
+            GraphicsObject.ReleaseHdc(DeviceContextHandle);
+            GraphicsObject.Dispose();
+            //Return the Scaling Factor
+            return ScreenScalingFactor;
+        }
+    }
+}
